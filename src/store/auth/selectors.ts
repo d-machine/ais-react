574695cb9d6969aae424db559f1ed00623f2be0f
Sumit Khaitan <sumitshark13@gmail.com>
2025-01-21 21:17:03 +0530

    authentication api

---
+++ src/store/auth/selectors.ts
@@ -0,0 +1,13 @@
+import { TAuthStore } from "./types";
+
+export function getIsLoggedIn(state: TAuthStore): boolean | undefined {
+  return state.isLoggedIn;
+}
+
+export function getToken(state: TAuthStore): string | undefined {
+  return state.token;
+}
+
+export function getRefreshToken(state: TAuthStore): string | undefined {
+  return state.refreshToken;
+}
---
+++ src/store/auth/types.ts
@@ -0,0 +1,5 @@
+export type TAuthStore = {
+  token?: string;
+  refreshToken?: string;
+  isLoggedIn?: boolean;
+}
