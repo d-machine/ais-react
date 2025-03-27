909944bbef13eef3b7e9f1c15fc939853fbdd7e5
Mohmad Ali <2021ucs0103@iitjammu.ac.in>
2025-03-27 16:57:00 +0530

    >

---
+++ src/components/Management/fetchData.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { postApiCall } from '../../api/base';
+import { IFetchQuery } from './types';
+
+export const fetchData = async (
+  url: string,
+  config: string,
+  setData: (name: string, data: any) => void,
+  initData: (name: string) => void,
+  setLoading: (loading: boolean) => void,
+  showSnackbar: (msg: string) => void,
+  name: string,
+  fetchQuery?: IFetchQuery,
+) => {
+  console.log(name, 'fetchQuery', fetchQuery);
+  initData(name);
+  try {
+    setLoading(true);
+    const response = await postApiCall(url, { configFile: config, fetchQuery }, true);
+    setData(name, response.data || []);
+  } catch (error) {
+    showSnackbar('Error fetching data');
+    console.error('Error fetching data:', error);
+  } finally {
+    setLoading(false);
+  }
+};
